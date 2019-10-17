00fca114190961758747c8eb6a705b420afda74b
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-10-17 14:30:49 +0200

    chore: Add enzyme for tests

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from "./App";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const app = shallow(<App />);
+
+it("renders the app correctly", () => {
+  expect(app).toMatchSnapshopt();
 });
